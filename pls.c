0c1fdfd459f54054107147b0f212b0856b476238
xeeny <xeeny@wp.pl>
2022-02-06 22:53:51 +0100

    Added pls.c

---
+++ pls.c
@@ -0,0 +1,18 @@
+#include <pwd.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main (int argc, char **argv) {
+    struct passwd *u = getpwnam("root");
+    if (argc < 2) {
+        fputs("usage: pls [comand]\n", stderr);
+        return 1;
+    }
+    if (setuid(u -> pw_uid) < 0) {
+        perror("setuid");
+        return 2;
+    }
+    execvp(argv[1], argv + 1);
+    perror("execvp");
+    return 2;
+}

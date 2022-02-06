832283105e542b33a35695654fc64a3c33c23b43
xeeny <xeeny@wp.pl>
2022-02-07 00:33:34 +0100

    Add password checking.

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ Run `install.sh` (as a root) add your user to groupe `pls` and you ready to go
 
 # Why?
 I was watching a [Brodie Robertson video](https://www.youtube.com/watch?v=7abih61ht-I)
-and then I thought, I can do better then that.
+and then I thought, I can do better than that.
 
 # Why it's can be actualy uesful.
 - As Brodie said is not popular softwere no one knows about it.
-- Depends on pretty match nothing (only libc).
+- Depends on pretty match nothing (only libc, and crypt).
 - Has no confing file (like suckless tools).
 - Is more suckless.
 - Is mote uinx (do one thing and do it very well).
--- pls.c
+++ pls.c
@@ -1,6 +1,37 @@
 #include <pwd.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <shadow.h>
+#include <string.h>
+#include <crypt.h>
+#include <termios.h>
+
+void check_pass(void) {
+    int i = 0;
+    struct termios oldt, newt;
+    struct spwd* spwd = getspnam(getlogin());
+    char passwd[512] = { 0 };
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    for (;; i++) {
+        fputs("password:", stderr);
+        fgets(passwd, sizeof(passwd), stdin);
+        *(strchr(passwd, '\n')) = 0;
+        if (strcmp(spwd->sp_pwdp, crypt(passwd, spwd->sp_pwdp)) == 0) {
+            tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+            fputs("\n", stderr);
+            return;
+        }
+        if (i < 3) break;
+        fputs("\nWrong password try again\n", stderr);
+    }
+    fputs("\nWrong password\n", stderr);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    exit(1);
+}
 
 int main (int argc, char **argv) {
     struct passwd *u = getpwnam("root");
@@ -8,6 +39,7 @@ int main (int argc, char **argv) {
         fputs("usage: pls [command]\n", stderr);
         return 1;
     }
+    check_pass();
     if (setuid(u -> pw_uid) < 0) {
         perror("setuid");
         return 2;
